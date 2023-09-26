9e66130918db1807d565623827873c8ad28240b2
guillaumeRequena <g.requena@laposte.net>
2023-09-26 21:59:21 +0200

    logout works

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -11,12 +11,15 @@ class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
-    {
-        // $error = $authenticationUtils->getLastAuthenticationError();        
-        // $lastUsername = $authenticationUtils->getLastUsername();        
+    {       
         return $this->render('login/index.html.twig', [
              'last_username' => $authenticationUtils->getLastUsername(),
              'error'         => $authenticationUtils->getLastAuthenticationError(),
         ]);        
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): never
+    {
     }
 }
