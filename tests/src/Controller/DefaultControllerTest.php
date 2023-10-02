8d5f366be74f2b0cb22d92ce242b24bc00ad29c2
guillaumeRequena <g.requena@laposte.net>
2023-10-02 23:23:57 +0200

    add test DefaultControllerTest

--- tests/src/Controller/DefaultControllerTest.php
+++ tests/src/Controller/DefaultControllerTest.php
@@ -2,8 +2,9 @@
 
 namespace Tests\App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultControllerTest extends WebTestCase
@@ -19,6 +20,27 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);        
         $this->assertEquals($url, $client->getResponse()->headers->get('Location'));        
+        
+        $client->followRedirect();
+        $this->assertEquals('app_login', $client->getRequest()->attributes->get('_route'));
     }
 
+    public function testIndexIfLogged(): void
+    {
+        $client = static::createClient();
+
+        // TODO - use fixtures ? create the user here ?
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneBy(['email' => 'test@test.test']);
+
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);       
+        $this->assertSame(1, $crawler->filter('h1')->count());
+        $this->assertEquals(
+            "Bienvenue sur Todo List, l'application vous permettant de gérer l'ensemble de vos tâches sans effort !", 
+            $crawler->filter('h1')->text()
+        );
+    }
 }
