c20618b939eff20345cfbb57541fec66a88bdba0
guillaumeRequena <g.requena@laposte.net>
2023-10-20 23:33:35 +0200

    add migrations

---
+++ migrations/Version20231014212414.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+
+final class Version20231014212414 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE user ADD roles JSON NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE user DROP roles');
+    }
+}
