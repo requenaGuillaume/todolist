4148db60c7efbe2565cb435a28beb3c2b6aadd9d
guillaumeRequena <g.requena@laposte.net>
2023-11-07 23:24:38 +0100

    test

--- migrations/Version20231021155525.php
+++ migrations/Version20231021155525.php
@@ -22,7 +22,6 @@ final class Version20231021155525 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE task MODIFY user_id INT DEFAULT NULL');
-             
+        $this->addSql('ALTER TABLE task MODIFY user_id INT DEFAULT NULL');             
     }
 }
